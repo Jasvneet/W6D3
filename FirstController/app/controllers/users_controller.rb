64c7e5400e33120a7d9b3992ff67031263731089
LetsGitFunky <NickHeinBass@gmail.com>
2023-05-03 17:12:13 -0700

    big fix and finalize

--- FirstController/app/controllers/users_controller.rb
+++ FirstController/app/controllers/users_controller.rb
@@ -1,15 +1,40 @@
 class UsersController < ApplicationController
 
     def index
-        render plain: "I'm in the index action!"
+        @users = User.all
+        render json: @users
     end
 
     def create
-        render json: params
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user
+        else
+            render json: @user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def show
-        render json: params
+        @user = User.find(params[:id])
+        render json: @user
     end
 
+    def destroy
+        @user = User.find(params[:id])
+        @user.delete
+        redirect_to users_url
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to user_url(@user)
+        else
+            redner json: @users.errors.full_messages, status: 422
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
 end
